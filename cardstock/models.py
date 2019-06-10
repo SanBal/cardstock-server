f370eb5ec687804a07b8cef47de7bf52cdb41d28
Sanjeev <sanjeev.b@gmx.de>
2019-06-10 13:42:33 +0200

    - Add further subclasses of 'Card' to models.py by using django-polymorphic package - Add 'polymorphic' to INSTALLED_APPS - Add update migration file

--- cardstock/models.py
+++ cardstock/models.py
@@ -1,12 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from polymorphic.models import PolymorphicModel
 
 
-class Card(models.Model):
-    TYPES = (
-        ('Monster', 'Monster'),
-        ('Spell', 'Spell'),
-        ('Trap', 'Trap')
-    )
+class Card(PolymorphicModel):
     RARITIES = (
         ('Common', 'Common'),
         ('Rare', 'Rare'),
@@ -15,9 +12,61 @@ class Card(models.Model):
         ('Ultra Rare', 'Ultra Rare'),
         ('Secret Rare', 'Secret Rare')
     )
+
     name = models.CharField(max_length=50)
-    type = models.CharField(max_length=7, choices=TYPES)
     rarity = models.CharField(max_length=15, choices=RARITIES)
 
+
+class MonsterCard(Card):
+    TYPES = (
+        ('Aqua', 'Aqua'),
+        ('Dinosaurier', 'Dinosaurier'),
+        ('Donner', 'Donner'),
+        ('Drache', 'Drache'),
+        ('Fee', 'Fee'),
+        ('Fels', 'Fels'),
+        ('Fisch', 'Fisch'),
+        ('Göttliches Ungeheuer', 'Göttliches Ungeheuer'),
+        ('Geflügeltes Ungeheuer', 'Geflügeltes Ungeheuer'),
+        ('Hexer', 'Hexer'),
+        ('Insekt', 'Insekt'),
+        ('Krieger', 'Krieger'),
+        ('Maschine', 'Maschine'),
+        ('Pflanze', 'Pflanze')
+    )
+
+    level = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(12)])
+    atk = models.PositiveSmallIntegerField()
+    defense = models.PositiveSmallIntegerField()
+    type = models.CharField(max_length=20, choices=TYPES)
+
     def __str__(self):
-        return "Card: {name: %s, type: %s, rarity: %s}" % (self.name, self.type, self.type)
+        return "MonsterCard: {name: %s, type: %s, atk: %d, def: %d, rarity: %s}" % (
+            self.name, self.type, self.atk, self.defense, self.rarity)
+
+
+class SpellCard(Card):
+    TYPES = (
+        ('Ausrüstung', 'Ausrüstung'),
+        ('Normal', 'Normal'),
+        ('Permanent', 'Permanent'),
+        ('Ritual', 'Ritual'),
+        ('Schnell', 'Schnell'),
+        ('Spielfeld', 'Spielfeld')
+    )
+    type = models.CharField(max_length=10, choices=TYPES)
+
+    def __str__(self):
+        return "SpellCard: {name: %s, type: %s, rarity: %s" % (self.name, self.type, self.rarity)
+
+
+class TrapCard(Card):
+    TYPES = (
+        ('Konter', 'Konter'),
+        ('Normal', 'Normal'),
+        ('Permanent', 'Permanent'),
+    )
+    type = models.CharField(max_length=10, choices=TYPES)
+
+    def __str__(self):
+        return "TrapCard: {name: %s, type: %s, rarity: %s" % (self.name, self.type, self.rarity)
