c4c2cadcd86174fd449271d07c7bb33aa062f854
Sanjeev <sanjeev.b@gmx.de>
2019-07-04 23:07:47 +0200

    Enable cors for local angular client

--- api/settings.py
+++ api/settings.py
@@ -36,11 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cardstock',
+    'corsheaders',
     'polymorphic',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200"
 ]
 
 ROOT_URLCONF = 'api.urls'
